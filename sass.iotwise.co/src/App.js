f1b45838b277f7a459f4fb0d432b160e657e76e0
E'Gonzalez <egonz993@gmail.com>
2023-11-09 09:19:00 -0500

    cms saas store www | folders create

---
+++ sass.iotwise.co/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h2>Software as a Service</h2>
+        <a
+          className="App-link"
+          href="https://www.iotwise.co"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>www.iotwise.co</p>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
