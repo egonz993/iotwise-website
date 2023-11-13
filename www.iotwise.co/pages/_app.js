3279356a779bda75991fa96eaf792771592b8b15
EGonzalez <ingenieria12@telemetrik.com.co>
2023-11-12 20:29:14 -0500

    html snippets for website

--- www.iotwise.co/pages/_app.js
+++ www.iotwise.co/pages/_app.js
@@ -25,11 +25,29 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="IoT Wise - Expertos en el Internet de las Cosas" />
+        <meta name="description" content="Expertos en el Internet de las Cosas (IoT)" />
         <meta name="keywords" content="iot, iiot, wireless, lorawan, lte, wisun, sigfox, smart city" />
         <meta name="author" content="E'Gonzalez" />
         
         <title>IoT Wise | Expertos en IoT</title>
+
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "http://schema.org",
+              "@type": "Website",
+              "headline": "IoT Wise | Expertos en IoT",
+              "image": {
+                "@type": "ImageObject",
+                "url": "https://iotwise.co/images/favicon.ico",
+                "width": 800,
+                "height": 600,
+                "alt": "IoT Wise | Expertos en el internet de las Cosas"
+              },
+              "description": "Expertos en el Internet de las Cosas"
+            }
+          `}
+        </script>
       </Head>
 
       <Component {...pageProps} />
