38d23ea08673d872e49ca8166612471fabb1c94b
E'Gonzalez <egonz993@gmail.com>
2024-04-09 21:57:35 -0500

    pagos link

---
+++ www.iotwise.co/pages/pagos.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Faq() {
+    React.useEffect(() => {
+        window.location.href = "https://cobros.global66.com/IOTWICO002"
+    }, [])
+    return <></>
+}
