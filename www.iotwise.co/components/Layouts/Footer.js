e19d2ca51ba48db91e7478be2d8e69e27cf00aff
E'Gonzalez <egonz993@gmail.com>
2024-04-09 21:49:33 -0500

    update footer links

--- www.iotwise.co/components/Layouts/Footer.js
+++ www.iotwise.co/components/Layouts/Footer.js
@@ -21,26 +21,31 @@ const Footer = () => {
 
                 <p>Síguenos en nuestras redes</p>
                 <ul className="social-icon">
-                  <li>
+                  {/* <li>
                     <a title="link to facebook" href="https://www.facebook.com/" target="_blank">
                       <i className="bx bxl-facebook"></i>
                     </a>
+                  </li> */}
+                  <li>
+                    <a title="link to whatsapp" className="icon-whatsapp" href="https://wa.me/573127897151" target="_blank">
+                      <i className="bx bxl-whatsapp"></i>
+                    </a>
                   </li>
                   <li>
-                    <a title="link to instagram" href="https://www.instagram.com/" target="_blank">
+                    <a title="link to instagram" className="icon-instagram" href="https://www.instagram.com/iotwise" target="_blank">
                       <i className="bx bxl-instagram"></i>
                     </a>
                   </li>
                   <li>
-                    <a title="link to youtube" href="https://www.youtube.com/" target="_blank">
+                    <a title="link to youtube" className="icon-youtube" href="https://www.youtube.com/@iotwise" target="_blank">
                       <i className="bx bxl-youtube"></i>
                     </a>
                   </li>
-                  <li>
+                  {/* <li>
                     <a title="link to linkedin" href="https://www.linkedin.com/" target="_blank">
                       <i className="bx bxl-linkedin"></i>
                     </a>
-                  </li>
+                  </li> */}
                 </ul>
               </div>
             </div>
@@ -55,76 +60,70 @@ const Footer = () => {
                 <h5 className="link-light">Qué ofrecemos</h5>
                 <ul>
                   <li>
-                    <Link href="/contact">
-                      <i className="right-icon bx bx-chevrons-right"></i>
-                      Asesoría y Capacitación
-                    </Link>
-                  </li>
-                  <li>
                     <Link href="https://store.iotwise.co" target="_blank">
                       <i className="right-icon bx bx-chevrons-right"></i>
                       Tienda Online
                     </Link>
                   </li>
                   <li>
-                    <Link href="https://senpai.app.iotwise.co" target="_blank">
+                    <Link href="/dev-hardware" target="_blank">
                       <i className="right-icon bx bx-chevrons-right"></i>
-                      IoT Senpai®
+                      Desarrollo de Hardware
                     </Link>
                   </li>
                   <li>
-                    <Link href="https://cloouder.app.iotwise.co" target="_blank">
+                    <Link href="/dev-software" target="_blank">
                       <i className="right-icon bx bx-chevrons-right"></i>
-                      Cloouder® LNS
+                      Desarrollo de Software
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/contact">
+                      <i className="right-icon bx bx-chevrons-right"></i>
+                      Asesoría y Capacitación
                     </Link>
                   </li>
                 </ul>
               </div>
             </div>
 
-<div className="col-lg-3 col-md-6">
-  <div
-    className="single-widget"
-    data-aos="fade-in"
-    data-aos-duration="1200"
-    data-aos-delay="400"
-  >
-    <h5 className="link-light">Enlaces de interés</h5>
+            <div className="col-lg-3 col-md-6">
+              <div
+                className="single-widget"
+                data-aos="fade-in"
+                data-aos-duration="1200"
+                data-aos-delay="400"
+              >
+                <h5 className="link-light">Enlaces de interés</h5>
 
-    <ul>
-      <li>
-        <Link href="/">
-          <i className="right-icon bx bx-chevrons-right"></i>
-          Inicio
-        </Link>
-      </li>
-      {/* <li>
-        <Link href="/about">
-          <i className="right-icon bx bx-chevrons-right"></i>
-          Nosotros
-        </Link>
-      </li> */}
-      <li>
-        <Link href="/services">
-          <i className="right-icon bx bx-chevrons-right"></i>
-          Servicios
-        </Link>
-      </li>
-      <li>
-        <Link href="/contact">
-          <i className="right-icon bx bx-chevrons-right"></i>
-          Contacto
-        </Link>
-      </li>
-      <li>
-        <Link href="/blog">
-          <i className="right-icon bx bx-chevrons-right"></i>
-          Blog
-        </Link>
-      </li>
-    </ul>
-  </div>
-</div>
+                <ul>
+                  <li>
+                    <Link href="/">
+                      <i className="right-icon bx bx-chevrons-right"></i>
+                      Inicio
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/about">
+                      <i className="right-icon bx bx-chevrons-right"></i>
+                      Nosotros
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/contact">
+                      <i className="right-icon bx bx-chevrons-right"></i>
+                      Contacto
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/blog">
+                      <i className="right-icon bx bx-chevrons-right"></i>
+                      Blog
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
 
             <div className="col-lg-3 col-md-6">
               <div
@@ -136,7 +135,7 @@ const Footer = () => {
                 <h5 className="link-light">Contacto</h5>
 
                 <ul className="information">
-                  
+
                   <li className="address">
                     <i className="flaticon-envelope"></i>
                     <span>Correo</span>
@@ -146,7 +145,8 @@ const Footer = () => {
                   <li className="address">
                     <i className="flaticon-maps-and-flags"></i>
                     <span>Ubicación</span>
-                    Medellín, Colombia
+                    <div>Medellín, Colombia</div>
+                    <div>Bello, Colombia</div>
                   </li>
                 </ul>
               </div>
@@ -166,12 +166,12 @@ const Footer = () => {
           <div className="row align-items-center">
             <div className="col-lg-4">
               <div className="copy-right">
-                <p>Copyright &copy; {currentYear} IoT Wise. All Rights Reserved</p>
+                <p>Copyright &copy; {currentYear} <Link href="https://www.iotwise.co/" >IoT Wise</Link>. All Rights Reserved</p>
               </div>
             </div>
 
-            <div className="col-lg-5">
-              <div className="privacy">
+            <div className="col-lg-8">
+              <div className="privacy text-end">
                 <ul>
                   <li>
                     <Link href="/terms-conditions">Terms & Conditions</Link>
@@ -179,18 +179,10 @@ const Footer = () => {
                   <li>
                     <Link href="/privacy-policy">Privacy Policy</Link>
                   </li>
+                  <li>
+                    <Link href="/legal-information">Legal Information</Link>
+                  </li>
                 </ul>
-              </div>
-            </div>
-
-            <div className="col-lg-3">
-              <div className="designed">
-                <p>
-                  Designed By {" "}
-                  <a href="https://egonzalez.web.app/" target="_blank">
-                    E'Gonzalez
-                  </a>
-                </p>
               </div>
             </div>
           </div>
